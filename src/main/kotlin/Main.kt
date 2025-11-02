41165a7300fa5733d34d8c894e3eae934e8dc5e2
Shanon Saga <shanonathooya@gmail.com>
2025-11-02 12:37:44 +0700

    add player code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,54 +1,13 @@
 package org.playground
 
-import org.playground.game.Player
-import org.playground.game.Scores
+import org.playground.game.game
 
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-//    forLoopCourse()
-//    score()
-//    println(factorial(2)) // 2
-//    println(factorial(3)) // 6
-    val player1 = Player("Nicola", "Tesla")
-    val player2 = Player("Thomas", "Edison")
-
-    val scoring = Scores()
-    var lvlScore = 0
-
-    // level 1
-    lvlScore = 20
-    player1.totalScore = scoring.totalScore(player1.totalScore, lvlScore)
-    player1.personalBestScore = scoring.bestAchieved(player1.personalBestScore, lvlScore)
-
-    lvlScore = 30
-    player2.totalScore = scoring.totalScore(player2.totalScore, lvlScore)
-    player2.personalBestScore = scoring.bestAchieved(player2.personalBestScore, lvlScore)
-
-    var highestScore = scoring.highestScore(player1, player2)
-    println(highestScore.winner())
-
-    // level 2
-    lvlScore = 50
-    player1.totalScore = scoring.totalScore(player1.totalScore, lvlScore)
-    player1.personalBestScore = scoring.bestAchieved(player1.personalBestScore, lvlScore)
-
-    lvlScore = 30
-    player2.totalScore = scoring.totalScore(player2.totalScore, lvlScore)
-    player2.personalBestScore = scoring.bestAchieved(player2.personalBestScore, lvlScore)
-
-    highestScore = scoring.highestScore(player1, player2)
-    println(highestScore.winner())
-
-    // level 3
-    lvlScore = 10
-    player1.totalScore = scoring.totalScore(player1.totalScore, lvlScore)
-    player1.personalBestScore = scoring.bestAchieved(player1.personalBestScore, lvlScore)
-
-    lvlScore = 35
-    player2.totalScore = scoring.totalScore(player2.totalScore, lvlScore)
-    player2.personalBestScore = scoring.bestAchieved(player2.personalBestScore, lvlScore)
-
-    highestScore = scoring.highestScore(player1, player2)
-    println(highestScore.winner())
+    forLoopCourse()
+    score()
+    println(factorial(2)) // 2
+    println(factorial(3)) // 6
+    val game = game()
 }
--- src/main/kotlin/game/Player.kt
+++ src/main/kotlin/game/Player.kt
@@ -11,3 +11,47 @@ class Player(
 
     fun winner(): String = "Winner ${fullName()} with a combined score of $totalScore and a personal best level score of $personalBestScore"
 }
+
+fun game() {
+    val player1 = Player("Nicola", "Tesla")
+    val player2 = Player("Thomas", "Edison")
+
+    val scoring = Scores()
+    var lvlScore = 0
+
+    // level 1
+    lvlScore = 20
+    player1.totalScore = scoring.totalScore(player1.totalScore, lvlScore)
+    player1.personalBestScore = scoring.bestAchieved(player1.personalBestScore, lvlScore)
+
+    lvlScore = 30
+    player2.totalScore = scoring.totalScore(player2.totalScore, lvlScore)
+    player2.personalBestScore = scoring.bestAchieved(player2.personalBestScore, lvlScore)
+
+    var highestScore = scoring.highestScore(player1, player2)
+    println(highestScore.winner())
+
+    // level 2
+    lvlScore = 50
+    player1.totalScore = scoring.totalScore(player1.totalScore, lvlScore)
+    player1.personalBestScore = scoring.bestAchieved(player1.personalBestScore, lvlScore)
+
+    lvlScore = 30
+    player2.totalScore = scoring.totalScore(player2.totalScore, lvlScore)
+    player2.personalBestScore = scoring.bestAchieved(player2.personalBestScore, lvlScore)
+
+    highestScore = scoring.highestScore(player1, player2)
+    println(highestScore.winner())
+
+    // level 3
+    lvlScore = 10
+    player1.totalScore = scoring.totalScore(player1.totalScore, lvlScore)
+    player1.personalBestScore = scoring.bestAchieved(player1.personalBestScore, lvlScore)
+
+    lvlScore = 35
+    player2.totalScore = scoring.totalScore(player2.totalScore, lvlScore)
+    player2.personalBestScore = scoring.bestAchieved(player2.personalBestScore, lvlScore)
+
+    highestScore = scoring.highestScore(player1, player2)
+    println(highestScore.winner())
+}
